ec3a899e8b0460e9b0423aa8c9ff0d77b76244cd
Chris Taylor <cctaylor@gmail.com>
2013-07-28 08:57:56 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ColorCraze::Application.config.secret_token = 'e9f9aeafefa615994c3b4b9663c321b702a0e5fdf2cd2a4fa60dbce675f9990b23e8eb267905a1ed08562d302639046e99a77b1071f0e4d5be1cdf8f11b8b21d'
