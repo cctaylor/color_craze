371eeb93a807203fc122a151c62184f4bbf7f698
Chris Taylor <cctaylor@gmail.com>
2013-07-29 07:48:31 -0400

    Add a StaticPages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def services
+  end
+
+  def customers
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 ColorCraze::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/services"
+
+  get "static_pages/customers"
+
+  get "static_pages/about"
+
+  get "static_pages/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
