be2133f07b19620e3fd6953935d97bcf25321cae
Chris Taylor <cctaylor@gmail.com>
2013-08-15 16:59:15 -0400

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :city, :company, :country, :email, :first_name, :last_name, :phone_m, :phone_o, :state, :street_1, :street_2, :zip, :password, :password_confirmation
   has_secure_password
+  has_many :microposts, dependent: :destroy
 
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
@@ -11,6 +12,11 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+
+  def feed
+    # This is preliminary. See "Following users" for the full implementation
+    Micropost.where("user_id = ?", id)
+  end
 
   private
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130808212930) do
+ActiveRecord::Schema.define(:version => 20130809205733) do
+
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "first_name"
